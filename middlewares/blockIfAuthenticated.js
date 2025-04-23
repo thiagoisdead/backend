bba8c69f5a458d2782a50148b3f78a8cbbf36d0b
Thiago Navarro <dollynhoextremeyt@gmail.com>
2025-04-22 23:07:17 -0300

    middleware pra bloquear acesso a pagina de login/signup caso já logado

---
+++ middlewares/blockIfAuthenticated.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET;
+
+const blockIfAuthenticated = (req, res, next) => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+  if (!token) return next(); 
+
+  try {
+    jwt.verify(token, jwtSecret);
+    return res.status(403).json({ message: 'Você já está logado. Não pode acessar esta página.' });
+  } catch {
+    return next(); 
+  }
+};
+
+module.exports = blockIfAuthenticated;
