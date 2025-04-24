0e88cbbe63dff9f1e98a11aab6b14c5b326480df
Thiago Navarro <dollynhoextremeyt@gmail.com>
2025-04-23 22:01:08 -0300

    reorganizando, mais nada

--- models/event.js
+++ models/event.js
@@ -12,21 +12,13 @@ const eventSchema = new mongoose.Schema({
     description: { type: String },
     date: { type: String, required: true },
     participants: { type: Number, required: true },
-    nickname: { type: String, required: true },
+    creatorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }
 }, {
     timestamps: true
 });
-eventSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    }
-    catch (err) {
-        next(err);
-    }
-})
 const Event = mongoose.model('Event', eventSchema);
 
 module.exports = Event;
