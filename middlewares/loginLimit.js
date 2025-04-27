51c89803e7ddf782e3b6392680dd1556c0a5017f
Thiago Navarro <dollynhoextremeyt@gmail.com>
2025-04-26 23:29:02 -0300

    middleware pra impedir ataques de força bruta no login/signup

---
+++ middlewares/loginLimit.js
@@ -0,0 +1,17 @@
+const rateLimit = require('express-rate-limit');
+
+const loginLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 6,
+    message: "Muitas tentativas de Login. Tente novamente após 10 minutos.",
+    standardHeaders: true, 
+    legacyHeaders: false, 
+})
+
+const registerLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, 
+    max: 15, 
+    message: "Muitas tentativas de Registro. Tente novamente após 10 minutos.",
+});
+
+module.exports = { loginLimiter, registerLimiter };
