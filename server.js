6a9ec3600ca5e5dce2e764ba5f14d33f4da080e6
Thiago Navarro <dollynhoextremeyt@gmail.com>
2025-05-03 13:01:21 -0300

    liberando cors pra deploy na vercel

--- server.js
+++ server.js
@@ -7,24 +7,37 @@ const app = express();
 const port = 3001;
 
 const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes'); 
+const authRoutes = require('./routes/authRoutes');
 
 app.use(helmet());
 app.use(helmet.frameguard({ action: 'SAMEORIGIN' }));
 
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://frontend-three-eosin-65.vercel.app'
+];
+
 app.use(cors({
-    origin: 'http://localhost:3000'
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
 }));
+
 
 app.use(express.json());
 
-app.use('/user', userRoutes); 
-app.use('/auth', authRoutes); 
+app.use('/user', userRoutes);
+app.use('/auth', authRoutes);
 
 
 connectToDb();
-app.listen(port, () => {console.log(`Servidor rodando em http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
 
 
